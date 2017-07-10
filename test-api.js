dd74656473ee6ac7a05f9726962e81c6d6cbfb21
VitalyMatyushkin <matushkin.vitaliy@gmail.com>
2017-07-11 00:04:01 +0600

    switch roles

--- test-api.js
+++ test-api.js
@@ -1,26 +1,67 @@
-$('body').append('<input type="text" id="sessionKeyInput" value="wx9f1ap2mmvwlx9hatp2xol5j6sqw7lyfhm2if3p">');
-$('body').append('<input type="submit" id="sessionKeySubmit" value="Login" onClick="sendsessionKey()">');
-$('body').append('<ul id="result">');
+$('body').append('<input type="text" id="sessionKeyInput" value="sozot87h9pizj2et7zvfcha0j4iyhihufsww7ikq">');
+$('body').append('<input type="submit" id="sessionKeySubmit" value="Login" onClick="checkData()">');
+$('body').append('<div id="result">');
 
 
-function sendsessionKey() {
+function checkData(){
+    getProfile();
+    getRoles();
+}
+
+function getProfile() {
     var usid = $("#sessionKeyInput").val();
-    var roles;
     $.ajax({
+        type: "GET",
+        headers: {"usid": usid},
+        url: "http://api.stage1.squadintouch.com/i/profile?filter=%22%22",
+        statusCode: {
+            200: function (response) {
+                $('#result').append('<div id="profile">');
+                $('#profile').html('Name: '+response.firstName + ' ' + response.lastName);
+            },
+        },
+        dataType: "json"
+    });
+}
+
+function getRoles() {
+    var usid = $("#sessionKeyInput").val();
+    var a = $.ajax({
         type: "GET",
         headers: {"usid": usid},
         url: "http://api.stage1.squadintouch.com/i/roles?filter=%22%22",
         statusCode: {
             200: function (response) {
-                roles = response;
-                roles.forEach(function(item) {
-                    console.log(item.name);
-                    $('#result').append("<li>"+item.name+"</li>");
+                response.forEach(function(item) {
+                    $('#result').append('<ul id="roles">');
+                    $('#roles').append("<li>"+item.name+"</li>");
                 });
+                switchRoles(response); //вызов функции переключения ролей
             },
         },
         dataType: "json"
     });
+}
 
+function switchRoles(roles) {
+    roles.forEach(function (item) {
+        setRole(item.name);
+    })
+}
+
+function setRole(role) {
+    var usid = $("#sessionKeyInput").val();
+    var roles;
+    $.ajax({
+        type: "POST",
+        headers: {"usid": usid},
+        url: "http://api.stage1.squadintouch.com/i/roles/" + role + "/become?filter=%22%22",
+        statusCode: {
+            201: function (response) {
+                console.log(response);
+            },
+        },
+        dataType: "json"
+    });
 }
 
