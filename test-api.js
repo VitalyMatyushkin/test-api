f5e617871b8a875bedb7917e30a1a792d74b8b68
VitalyMatyushkin <matushkin.vitaliy@gmail.com>
2017-07-11 11:52:06 +0600

    get role lists

--- test-api.js
+++ test-api.js
@@ -1,16 +1,31 @@
-$('body').append('<input type="text" id="sessionKeyInput" value="sozot87h9pizj2et7zvfcha0j4iyhihufsww7ikq">');
+$('body').append('<input type="text" id="sessionKeyInput" value="e4mc48qmxb7vsps3e5y0lv00jjtax02722exjn0v">');
 $('body').append('<input type="submit" id="sessionKeySubmit" value="Login" onClick="checkData()">');
 $('body').append('<div id="result">');
 
 
 function checkData(){
-    getProfile();
-    getRoles();
+    getProfile()
+        .then(function () {
+        return getRoles()
+    })
+        .then(function (roles) {
+            roles.forEach(function (item) {
+                setRole(item.name)
+                    .then(function (role) {
+                    getSchools(role.key).then();
+                    if (role.role === 'ADMIN' || role.role === 'MANAGER' || role.role === 'TRAINER'){
+                        getStudentList(role.key);
+                        getHouseList(role.key);
+                        getFormList(role.key);
+                    }
+                })
+            });
+        });
 }
 
 function getProfile() {
     var usid = $("#sessionKeyInput").val();
-    $.ajax({
+    return $.ajax({
         type: "GET",
         headers: {"usid": usid},
         url: "http://api.stage1.squadintouch.com/i/profile?filter=%22%22",
@@ -26,7 +41,7 @@ function getProfile() {
 
 function getRoles() {
     var usid = $("#sessionKeyInput").val();
-    var a = $.ajax({
+    return $.ajax({
         type: "GET",
         headers: {"usid": usid},
         url: "http://api.stage1.squadintouch.com/i/roles?filter=%22%22",
@@ -36,28 +51,50 @@ function getRoles() {
                     $('#result').append('<ul id="roles">');
                     $('#roles').append("<li>"+item.name+"</li>");
                 });
-                switchRoles(response); //вызов функции переключения ролей
             },
         },
         dataType: "json"
     });
 }
 
-function switchRoles(roles) {
-    roles.forEach(function (item) {
-        setRole(item.name);
-    })
-}
-
 function setRole(role) {
     var usid = $("#sessionKeyInput").val();
-    var roles;
-    $.ajax({
+    return $.ajax({
         type: "POST",
         headers: {"usid": usid},
         url: "http://api.stage1.squadintouch.com/i/roles/" + role + "/become?filter=%22%22",
         statusCode: {
             201: function (response) {
+                // console.log(response);
+            },
+        },
+        dataType: "json"
+    });
+}
+
+function getSchools(sessionKey) {
+    return $.ajax({
+        type: "GET",
+        headers: {"usid": sessionKey},
+        url: "http://api.stage1.squadintouch.com/i/schools?filter=%22%22",
+        statusCode: {
+            200: function (response) {
+                // console.log(response);
+            },
+        },
+        dataType: "json"
+    });
+}
+
+
+
+function getStudentList(sessionKey) {
+    return $.ajax({
+        type: "GET",
+        headers: {"usid": sessionKey},
+        url: "http://api.stage1.squadintouch.com/i/schools/57b6c9a6dd69264b6c5ba82f/students?filter=%7B%22limit%22%3A20%2C%22skip%22%3A20%7D&{}",
+        statusCode: {
+            200: function (response) {
                 console.log(response);
             },
         },
@@ -65,3 +102,30 @@ function setRole(role) {
     });
 }
 
+function getHouseList(sessionKey) {
+    return $.ajax({
+        type: "GET",
+        headers: {"usid": sessionKey},
+        url: "http://api.stage1.squadintouch.com/i/schools/57b6c9a6dd69264b6c5ba82d/houses?filter=%7B%22limit%22%3A20%7D&{}",
+        statusCode: {
+            200: function (response) {
+                console.log(response);
+            },
+        },
+        dataType: "json"
+    });
+}
+
+function getFormList(sessionKey) {
+    return $.ajax({
+        type: "GET",
+        headers: {"usid": sessionKey},
+        url: "http://api.stage1.squadintouch.com/i/schools/57b6c9a6dd69264b6c5ba82d/forms?filter=%7B%22limit%22%3A30%7D&{}",
+        statusCode: {
+            200: function (response) {
+                console.log(response);
+            },
+        },
+        dataType: "json"
+    });
+}
